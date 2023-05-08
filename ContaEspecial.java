8825af30dd183e5dab8e862a22a83d36ad9031c6
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-05-08 19:37:41 -0300

    Resolução das questões 1 e 2

---
+++ ContaEspecial.java
@@ -0,0 +1,26 @@
+public class ContaEspecial extends Conta {
+    private double limite;
+    
+    public ContaEspecial(int numero, Cliente titular, double limite){
+        super(numero, titular);
+        this.limite = limite;
+    }
+    
+    public double getLimite(){
+        return limite;
+    }
+    
+    public void setLimite(double limite){
+        this.limite = limite;
+    }
+    
+    @Override
+    public boolean sacar(double valor){
+        if(valor <= this.limite + this.saldo){
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
